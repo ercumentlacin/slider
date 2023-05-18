77c0a168db44becec434ea518ad04d648fc455e6
ercumentlacin <forever.ducale@gmail.com>
2023-05-18 14:31:16 +0300

    include global.scss

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.tsx'
 import { store } from './app/store.ts'
-import './index.css'
+import './styles/global.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
