ace34e5596c8fc634f224c75fae21da0693f50cc
ercumentlacin <forever.ducale@gmail.com>
2023-05-18 19:36:43 +0300

    Refactor screenSlice.ts to use clearer window value names and add corresponding selector functions

--- src/features/screen/screenSlice.ts
+++ src/features/screen/screenSlice.ts
@@ -1,40 +1,49 @@
+import type { RootState } from '@/app/store';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
-import type { RootState } from '@/app/store';
 
-// Define a type for the slice state
 interface ScreenState {
-  x: number;
-  y: number;
+  horizontalWindowValues: number[];
+  topVerticalWindowValues: number[];
+  bottomVerticalWindowValues: number[];
 }
 
-// Define the initial state using that type
 const initialState: ScreenState = {
-  x: 0,
-  y: 0,
+  horizontalWindowValues: [50, 50],
+  topVerticalWindowValues: [50, 50],
+  bottomVerticalWindowValues: [50, 50],
 };
 
 export const screenSlice = createSlice({
   name: 'screen',
-  // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    increment: (state) => {
-      state.x += 1;
+    changeHorizontalWindowValues: (state, action: PayloadAction<number[]>) => {
+      state.horizontalWindowValues = action.payload;
     },
-    decrement: (state) => {
-      state.x -= 1;
+    changeTopVerticalWindowValues: (state, action: PayloadAction<number[]>) => {
+      state.topVerticalWindowValues = action.payload;
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.x += action.payload;
+    changeBottomVerticalWindowValues: (
+      state,
+      action: PayloadAction<number[]>,
+    ) => {
+      state.bottomVerticalWindowValues = action.payload;
     },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = screenSlice.actions;
+export const {
+  changeHorizontalWindowValues,
+  changeTopVerticalWindowValues,
+  changeBottomVerticalWindowValues,
+} = screenSlice.actions;
 
-// Other code such as selectors can use the imported `RootState` type
-export const selectScreenX = (state: RootState) => state.screen.x;
+export const selectHorizontalWindowValues = (state: RootState) =>
+  state.screen.horizontalWindowValues;
+export const selectTopVerticalWindowValues = (state: RootState) =>
+  state.screen.topVerticalWindowValues;
+export const selectBottomVerticalWindowValues = (state: RootState) =>
+  state.screen.bottomVerticalWindowValues;
 
 export default screenSlice.reducer;
