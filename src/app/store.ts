3abede341f26bbd990da98e97b9fd5a929630677
ercumentlacin <forever.ducale@gmail.com>
2023-05-19 11:22:01 +0300

    add redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
 import screenReducer from '@/features/screen/screenSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'screen',
+  storage,
+};
+
+const persistedScreenReducer = persistReducer(persistConfig, screenReducer);
 
 export const store = configureStore({
   reducer: {
-    screen: screenReducer,
+    screen: persistedScreenReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import '@total-typescript/ts-reset';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './app/store.ts';
 import App from './features/App.tsx';
 import './styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 );
